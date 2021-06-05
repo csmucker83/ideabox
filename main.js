fa9feb28d19fa83dcd754a575b13a7ee2590a1b9
Lindsay Schnell <lschnell8@gmail.com>
2021-06-05 13:51:55 -0600

    Create functionality for save button

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 var ideas = [];
+//QuerySelectors
+var titleInput = document.getElementById('titleInput')
+var saveBtn = document.getElementById('saveBtn');
+var bodyInput = document.getElementById('bodyInput');
+//EventListeners
+saveBtn.addEventListener('click', function(event) {
+  createNewIdea(event);
+});
+
+//Functions and Event Handlers
+function createNewIdea(event) {
+  event.preventDefault();
+  var newIdea = new Idea(titleInput.value, bodyInput.value);
+  if (!titleInput.value || !bodyInput.value) {
+    saveBtn.disabled
+  } else {
+    ideas.push(newIdea);
+    clearInputFields();
+  }
+};
+
+
+function clearInputFields() {
+  titleInput.value = '';
+  bodyInput.value = '';
+};
